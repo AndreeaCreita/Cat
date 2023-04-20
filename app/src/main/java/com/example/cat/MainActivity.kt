84c6840091ff8ea6af3316487c9f9d20274d8992
Andreea Creita <andreea_creita@yahoo.com>
2023-04-20 15:48:14 +0300

    Register Activity works

--- app/src/main/java/com/example/cat/MainActivity.kt
+++ app/src/main/java/com/example/cat/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -20,8 +21,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    //
 
-
+    //register
+    private lateinit var createnewAccount: TextView
     //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +43,17 @@ class MainActivity : AppCompatActivity() {
             signInGoogle()
         }
         //
+
+        //Register
+
+        createnewAccount = findViewById(R.id.createNewAccount)
+
+        createnewAccount.setOnClickListener {
+            startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+        }
     }
 
-    //google
+//google
     private fun signInGoogle(){
 
         var signInIntent = googleSignInClient.signInIntent
@@ -85,6 +96,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+//  ---google
+
 
 }
 
---
+++ app/src/main/java/com/example/cat/RegisterActivity.kt
@@ -0,0 +1,56 @@
+package com.example.cat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var alreadyhaveAccount: TextView
+    lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        auth = FirebaseAuth.getInstance()
+
+        alreadyhaveAccount = findViewById(R.id.alreadyHaveAccount)
+
+        alreadyhaveAccount.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+        }
+        findViewById<Button>(R.id.btnRegister).setOnClickListener {
+            registerUser()
+        }
+    }
+    private fun registerUser() {
+        val email = findViewById<EditText>(R.id.inputEmail).text.toString()
+        val password = findViewById<EditText>(R.id.inputPassword).text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password).await()
+
+                    withContext(Dispatchers.Main) {
+
+                        Log.d("TAG", "This is a debug message")
+
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@RegisterActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+}
