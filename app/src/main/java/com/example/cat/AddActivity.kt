e9f1b480e479395e6f4d1c1801e4acdcde63bd3c
Andreea Creita <andreea_creita@yahoo.com>
2023-04-21 15:34:48 +0300

    Implemented Add_activity layout

--- app/src/main/java/com/example/cat/AddActivity.kt
+++ app/src/main/java/com/example/cat/AddActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
+//import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
 
